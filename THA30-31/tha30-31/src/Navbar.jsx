03c2d97996766661463c24b1fd6a0ec55ab8f4f0
Rahul Singh Bisht <93148343+hikigaya113@users.noreply.github.com>
2022-10-01 13:40:52 +0530

    Add files via upload

---
+++ THA30-31/tha30-31/src/Navbar.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import "./Navbar.css";
+import { auth } from './firebase';
+import { useSelector, useDispatch } from 'react-redux';
+import {setUserLogoutState,selectUserName } from './features/userSlice';
+import SearchIcon from "@material-ui/icons/Search";
+import { Button } from '@material-ui/core';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import AllInclusiveIcon from '@material-ui/icons/AllInclusive';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import { NavLink } from 'react-router-dom';
+
+
+
+const Navbar = ({setIslogin}) => {
+  const dispatch = useDispatch();
+  const username = useSelector(selectUserName);
+  const handleSignOut = ()=>{
+    auth.signOut().then(()=>{
+      dispatch(setUserLogoutState());
+      setIslogin(null);
+      localStorage.setItem('name','');
+    }).catch((err)=>alert(err.message))
+  }
+    return (
+        <div>
+            <nav className="header">
+                <div className="title">
+                    <h2><span className="num">D</span>evsCart <AllInclusiveIcon style={{fontSize: "35"}} /></h2>   
+                </div>
+
+          <div className="categories"> 
+                <NavLink to="/home" style={{textDecoration:'none',color:'black'}}> <div><span className="item">Home</span></div></NavLink> 
+                <a href="#categories" style={{textDecoration:'none',color:'black'}}>   <div><span className="item">Categories</span></div> </a>
+                    <div><span className="item">Accessories</span></div>
+                </div>
+               
+                <div className="widgets__input">
+        <SearchIcon className="widgets__searchIcon" />
+        <input placeholder="Search DevsCart" type="text" />
+      </div>
+                <div className='header__right'>
+                <div>
+                        <Button><AddShoppingCartIcon style={{fontSize: "30px"}} /></Button>
+                        <Button><FavoriteIcon style={{fontSize: "30px", color: "red"}} /></Button>
+                    </div>
+                    <div>
+                        <Button style={{marginLeft: '10px', backgroundColor: "black", border: '0.5px solid black', fontWeight: '600'}} size="small" variant="contained" color="primary" onClick={handleSignOut}>LogOut</Button>
+                        
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar;
